70ee232820fa414f57c0d6657629eb55fb659052
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-05-16 09:39:55 -0400

    additional logging for clearTimeout

--- src/compiler/transpile/create-build-program.ts
+++ src/compiler/transpile/create-build-program.ts
@@ -3,6 +3,12 @@ import { getTsOptionsToExtend } from './ts-config';
 import { GENERATED_DTS } from '../output-targets/output-utils';
 import ts from 'typescript';
 
+/**
+ *
+ * @param config
+ * @param buildCallback
+ * @returns
+ */
 export const createTsBuildProgram = async (
   config: d.Config,
   buildCallback: (tsBuilder: ts.BuilderProgram) => Promise<void>
@@ -31,24 +37,28 @@ export const createTsBuildProgram = async (
         close() {},
       };
     },
-    setTimeout(callback, time) {
+    setTimeout(callback: (...args: any[]) => void , watchTimeoutMs: number) {
       timeoutId = setInterval(() => {
         if (!isRunning) {
+          console.trace(`src/compiler/transpile/create-build-program.ts#setTimeout - isRunning: false`)
           callback();
           clearInterval(timeoutId);
+        } else {
+          console.trace(`src/compiler/transpile/create-build-program.ts#setTimeout - isRunning: true`)
         }
-      }, config.sys.watchTimeout || time);
+      }, config.sys.watchTimeout || watchTimeoutMs); // TODO: Probably a bug if config.sys.watchTimeout is 0
       return timeoutId;
     },
 
     clearTimeout(id) {
+      console.trace(`src/compiler/transpile/create-build-program.ts#setTimeout - clearTimeout: ` + id)
       return clearInterval(id);
     },
   };
 
   config.sys.addDestory(() => tsWatchSys.clearTimeout(timeoutId));
 
-  const tsWatchHost = ts.createWatchCompilerHost(
+  const tsWatchHost: ts.WatchCompilerHostOfConfigFile<ts.EmitAndSemanticDiagnosticsBuilderProgram> = ts.createWatchCompilerHost(
     config.tsconfig,
     optionsToExtend,
     tsWatchSys,
@@ -62,13 +72,15 @@ export const createTsBuildProgram = async (
   );
 
   const id1 = Math.ceil(Math.random() * 1_000_000);
-  tsWatchHost.afterProgramCreate = async (tsBuilder) => {
+  tsWatchHost.afterProgramCreate = async (tsBuilder: ts.EmitAndSemanticDiagnosticsBuilderProgram) => {
     const id2 = Math.ceil(Math.random() * 1_000_000);
+    console.trace('src/compiler/transpile/create-build-program.ts#afterProgramCreate - setting isRunning: true', id1, id2)
     isRunning = true;
     console.trace('src/compiler/transpile/create-build-program.ts#afterProgramCreate', id1, id2)
     await buildCallback(tsBuilder);
-    console.trace('src/compiler/transpile/create-build-program.ts#afterProgramCreate - COMPLETE', id1, id2)
+    console.trace('src/compiler/transpile/create-build-program.ts#afterProgramCreate - setting isRunning: false', id1, id2)
     isRunning = false;
+    console.trace('src/compiler/transpile/create-build-program.ts#afterProgramCreate - COMPLETE', id1, id2)
   };
   console.trace('src/compiler/transpile/create-build-program.ts#createWatchProgram', id1)
   return ts.createWatchProgram(tsWatchHost);
